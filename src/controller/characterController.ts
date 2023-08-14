ff208c0c0b750cdff9a589ff54bc6a86b4b35704
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-14 17:47:12 +0200

    feat: basic setup controller

---
+++ src/controller/characterController.ts
@@ -0,0 +1,13 @@
+import { db } from '../model/characterModel';
+import { Character } from '../model/type';
+
+async function createNewCharacter(character: Character) {
+
+}
+
+async function updateCharacterData(character: Character) {
+
+}
+
+
+export { createNewCharacter, updateCharacterData }
