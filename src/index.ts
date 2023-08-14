19f1ddf47e663508919bb37496c654abb311bf89
Chris Sorensen <chris_sorensen@live.com>
2023-08-14 14:39:03 +0200

    Prisma setup, schema defined

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-import express, { Express, Request, Response } from 'express';
-// import cors from 'cors';
 import dotenv from 'dotenv';
+dotenv.config();
+import express, { Express } from 'express';
 
 const app: Express = express();
-
-dotenv.config();
-
 const PORT: string = process.env.PORT || '3000';
 
-// app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Arto!');
-});
-
-
-app.listen(Number(PORT), '0.0.0.0', () => {
+app.listen(Number(PORT), '127.0.0.1', () => {
   console.log(`Server listening on port ${PORT}`);
 });
