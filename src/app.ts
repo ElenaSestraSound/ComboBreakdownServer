69c7d634644398688bbe9022ea6e9a72a0970898
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-14 16:44:15 +0200

    feat: added error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import { ErrorHandler } from './middleware/errorHandler';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(ErrorHandler);
 
 // test
 app.get('/', (req: Request, res: Response) => {
