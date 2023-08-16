886c000ec25a8b75b99783ccd43bc6a28c8780c0
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-16 18:01:36 +0200

    refactor: put error handler on end of file

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,17 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { ErrorHandler } from './middleware/errorHandler';
 import { router } from './scraper/routes';
+import { prisma } from './prisma/client';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(ErrorHandler);
 
 // test
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-export { app };
+app.use(ErrorHandler);
