e633d8d7ce8b66f9d8bb04bfa8957ca5605f1720
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-16 12:18:58 +0200

    fix: fixed names for url endings

--- src/scraper/loopThroughUrls.mjs
+++ src/scraper/loopThroughUrls.mjs
@@ -1,17 +1,18 @@
 import puppeteer from 'puppeteer';
+import { scrapeCharacterStats } from './characterPage/characterScrape.mjs';
 
 const baseUrl = 'https://www.streetfighter.com/6/character/';
 const characters = [
-  'rashid', 'cammy', 'lily', 'zangief', 'jp', 'marisa', 'manon', 'deejay', 'honda',
-  'dhalsim', 'blanka', 'ken', 'juri', 'kimberly', 'guile', 'chun-li', 'jaime', 'luke', 'ryu'
+  'rashid', 'cammy', 'lily', 'zangief', 'jp', 'marisa', 'manon', 'deejay', 'ehonda',
+  'dhalsim', 'blanka', 'ken', 'juri', 'kimberly', 'guile', 'chunli', 'jamie', 'luke', 'ryu'
 ];
 
-const charactersObject = characters.reduce((obj, character) => {
+const charactersUrlObject = characters.reduce((obj, character) => {
   obj[character] = baseUrl + character;
   return obj;
 }, {});
 
-console.log(charactersObject);
+console.log(charactersUrlObject);
 
 const paths = ['/movelist', '/frame'];
 
@@ -22,18 +23,48 @@ const paths = ['/movelist', '/frame'];
 
 // Also keep in mind that the modern controls are behind a button click
 
+// async function processPage(url) {
+  
+//   console.log(`Processing ${url}`);
+
+//   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+//   const page = await browser.newPage();
+//   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36');
+//   await page.goto(url);
+//   const data = await page.evaluate(() => {
+//     const result = scrapeCharacterStats();
+//     return result;
+//   }
+//   console.log(data);
+// }
+
 async function processPage(url) {
-  // Here, write the function you want to execute on each page
+  
   console.log(`Processing ${url}`);
+
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36');
+  await page.goto(url);
+
+  const data = await page.evaluate(() => {
+
+    return {};
+  });
+
+  console.log(data);
+
+  await browser.close();
 }
+
 
 (async () => {
   const browser = await puppeteer.launch();
 
-  for (const character in charactersObject) {
+  for (const character in charactersUrlObject) {
     const page = await browser.newPage();
-    await page.goto(charactersObject[character]);
-    await processPage(charactersObject[character]);
+    await page.goto(charactersUrlObject[character]);
+    await processPage(charactersUrlObject[character]);
     await page.close();
   }
 
