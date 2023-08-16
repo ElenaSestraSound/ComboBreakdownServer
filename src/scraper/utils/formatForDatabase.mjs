73637cf4be583dd11eceee447a8b72c29bab25f6
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-16 17:39:01 +0200

    refactor: updated formatMoves function

--- src/scraper/utils/formatForDatabase.mjs
+++ src/scraper/utils/formatForDatabase.mjs
@@ -1,27 +1,37 @@
-function formatMoves (data) {
+function formatMoves(data) {
+
+  if (
+    !data.name || typeof data.name !== "string" ||
+    !data.type || typeof data.type !== "string" ||
+    !data.classic || typeof data.classic !== "string"
+  ) {
+    throw new Error("Invalid or missing required fields (name, type, classic).");
+  }
+
   return {
-      name: data.name,
-      type: data.type,
-      driveGauge: data.driveGauge ? parseInt(data.driveGauge, 10) : null,
-      video: data.video || null,
-      definition: data.definition || null,
-      classic: data.classic,
-      modern: data.modern || "",
-      manual: data.manual || null,
-      startup: parseInt(data.startup, 10),
-      active: data.active || "",
-      missRecovery: parseInt(data.missRecovery, 10),
-      hitStunRecovery: parseInt(data.hitStunRecovery, 10),
-      blockStunRecovery: parseInt(data.blockStunRecovery, 10),
-      cancelable: data.cancelable || "",
-      damage: parseInt(data.damage, 10),
-      driveIncreaseHit: parseInt(data.driveIncreaseHit, 10),
-      driveDecreaseBlock: parseInt(data.driveDecreaseBlock, 10),
-      driveDecreasePunish: parseInt(data.driveDecreasePunish, 10),
-      superArtGaugeIncrease: parseInt(data.superArtGaugeIncrease, 10),
-      properties: data.properties,
-      scaling: data.scaling && data.scaling.length ? parseInt(data.scaling[0], 10) : null
+    name: data.name,
+    type: data.type,
+    driveGauge: data.driveGauge || null,
+    video: data.video || null,
+    definition: data.definition || null,
+    classic: data.classic,
+    modern: data.modern || null,
+    manual: data.manual || null,
+    startup: data.startup || null,
+    active: data.active || null,
+    missRecovery: data.missRecovery || null,
+    hitStunRecovery: data.hitStunRecovery || null,
+    blockStunRecovery: data.blockStunRecovery || null,
+    cancelable: data.cancelable || null,
+    damage: data.damage || null,
+    driveIncreaseHit: data.driveIncreaseHit || null,
+    driveDecreaseBlock: data.driveDecreaseBlock || null,
+    driveDecreasePunish: data.driveDecreasePunish || null,
+    superArtGaugeIncrease: data.superArtGaugeIncrease || null,
+    properties: data.properties || null,
+    scaling: data.scaling || null
   };
+
 }
 
 export { formatMoves };
