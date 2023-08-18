404e4fe579c1e14af1d92585c514b8d0710217e3
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-18 10:40:18 +0200

    feat: added function to scrape command list page

--- src/scraper/index.mjs
+++ src/scraper/index.mjs
@@ -49,36 +49,27 @@ async function processFrameDataPage(url) {
   };
 }
 
+async function processCommandListPage(url) {
+  
+  console.log(`Processing ${url}`);
+
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36');
+  await page.goto(url);
+
+  // const data = await page.evaluate(`(${scrapeCharacterStats.toString()})();`);
+
+  await browser.close();
+  return data;
+}
+
 /* scrape function */
 
 const getScrapeData = async () => {
 
   const characterMap = new Map();
   const browser = await puppeteer.launch();
-
-  // for (const character in charactersUrlObject) {
-  //   const page = await browser.newPage();
-  //   await page.goto(charactersUrlObject[character]);
-  //   const data = await processCharacterPage(charactersUrlObject[character]);
-  //   characterMap.set(data.name, data);
-  //   break;
-  //   await page.close();
-  // }
-  
-  // for (const character in charactersUrlObject) {
-  //   const page = await browser.newPage();
-  //   await page.goto(charactersUrlObject[character] + '/frame');
-  //   const result = await processFrameDataPage(charactersUrlObject[character] + '/frame'); 
-  //   if(characterMap.has(character)) {
-  //     const characterObject = characterMap.get(character);
-  //     characterObject.moves = result.data;
-  //     characterObject.vitality = result.vitality;
-  //   }
-  //   break;
-  //   await page.close();
-  // }
-
-
 
   for (const character in charactersUrlObject) {
     
@@ -108,13 +99,10 @@ const getScrapeData = async () => {
   }
 
 
-
-
   await browser.close();
 
   const characterArray = [...characterMap.values()];
 
-  // console.log(characterArray);
   return characterArray;
 };
 
