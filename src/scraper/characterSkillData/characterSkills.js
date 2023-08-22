56d57a18947f35acb87a61eb2dfd5a52165a5489
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-22 08:10:44 +0200

    feat: generate videoUrls

--- src/scraper/characterSkillData/characterSkills.js
+++ src/scraper/characterSkillData/characterSkills.js
@@ -1,5 +1,5 @@
-// import { rawSkillData } from './rawSkillData.js';
-// import fs from 'fs';
+import { rawSkillData } from './rawSkillData.js';
+import fs from 'fs';
 
 /* helper functions to format data */
 
@@ -8,17 +8,26 @@ const filterInvalidValues = (obj) => {
   return Object.fromEntries(
       Object.entries(obj).filter(([_, value]) => !pattern.test(value))
   );
-}
+};
 
 const filterKeyString = (string) => {
   const regex = /\[t\](skill_.*?_)(.*?)_(\d{3})/;
   return string.match(regex);
-}
+};
 
 const filterPathString = (string) => {
   const regex = /[^/]+$/;
   return string.match(regex)[0];
-}
+};
+
+function generateVideoUrl(name, videoId) {
+  if (!videoId) {
+      return null;
+  }
+  const baseURL = "https://www.streetfighter.com/6/assets/images/character/";
+  return `${baseURL}${name}/skill/movie/${videoId}.mp4`;
+};
+
 
 /* refactor skills/moves data */
 
@@ -32,12 +41,13 @@ const transformSkillData = (data) => {
     const match = filterKeyString(key)
     if (match) {
       const type = match[1];
+      const character = match[2];
       const id = match[3];
 
       if (type === "skill_name_") {
           result.push({
               name: data[key],
-              video: id
+              video: generateVideoUrl(character, id)
           });
       } else if (type === "skill_comment_") {
           const skill = result.find(skill => skill['video'] === id);
@@ -61,11 +71,11 @@ function transformCharacterData(data) {
   });
 }
 
-// let data = await transformCharacterData(rawSkillData);
-// fs.writeFile('output.json', JSON.stringify(data), (err) => {
-//  if (err) throw err;
-// });
+let data = await transformCharacterData(rawSkillData);
+fs.writeFile('output.json', JSON.stringify(data), (err) => {
+ if (err) throw err;
+});
 
-// console.log(transformCharacterData(rawSkillData))
+console.log(transformCharacterData(rawSkillData))
 
 export { transformCharacterData };
