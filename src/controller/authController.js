e02bb3f1ac3fc4c1b005efcf0a3e4db48562e9e4
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-23 11:40:21 +0200

    feat: basic setup for authentication

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,3 +1,6 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 
 /* *** GET *** */
 /* Returns a login form with only a password field */
@@ -18,6 +21,30 @@ const passwordAdmin = async (req, res, next) => {
 
 const loginAdmin = async (req, res, next) => {
   try {
+
+    /* TEMPLATE */
+
+    // Dummy data
+    const users = [{ email: "vincent@vincentlab.net", password: "$2b$15$zqY2Q4eOoGzFpZkHJz9HS.BSfXc/HM2E/yTWa1awFmTMgN2bE72Uu", roles: ["admin", "editor", "viewer"] }];
+
+    // Get to user from the database, if the user is not there return error
+    let user = users.find(u => u.email === req.body.email);
+    if (!user) throw new Error("Invalid email or password.");
+
+    // Compare the password with the password in the database
+    const valid = await bcrypt.compare(req.body.password, user.password)
+    if (!valid) throw new Error("Invalid email or password.");
+
+    const token = jwt.sign({
+        id: user._id,
+        roles: user.roles,
+    }, "jwtPrivateKey", { expiresIn: "30m" });
+
+    res.send({
+        ok: true,
+        token: token
+    });
+
 
   } catch (err) {
     next(err);
