d25bd6cdf454d7e2b703a97a8f6261a4fd2bf65d
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-23 11:32:25 +0200

    feat: added authController and routes

---
+++ src/controller/authController.js
@@ -0,0 +1,27 @@
+
+/* *** GET *** */
+/* Returns a login form with only a password field */
+/* When you are already logged in redirects to /scrape */
+/* The form does a POST request to /admin */
+
+const passwordAdmin = async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+/* *** POST *** */
+/* Logs in the admin user */
+
+const loginAdmin = async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { passwordAdmin, loginAdmin };
--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getAllCharacterData, postSpecificCharacterData } from './controller/scrapeController.js';
+import { loginAdmin, passwordAdmin } from './controller/authController.js';
 
 const router = express.Router();
 
 /* authentication routes */
-router.get('/auth/admin');
-router.post('/auth/admin');
+router.get('/auth/admin', passwordAdmin);
+router.post('/auth/admin', loginAdmin);
 
 /* scraping routes */
 router.get('/scrape/all', getAllCharacterData);
